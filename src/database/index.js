72cc3124a11a3a58a7753c0d51a0bca7c5518e0b
Jackson da Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2022-10-15 18:00:29 -0300

    model User added in models

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 const dbConfig = require('../config/database');
+const User = require('../models/User');
 
 const sequelize = new Sequelize(dbConfig);
+
+User.init(sequelize);
 
 module.exports = sequelize;
---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const { DataTypes, Model } = require('sequelize');
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+      },
+    );
+  }
+}
+
+module.exports = User;
